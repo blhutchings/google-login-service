d8e638c8fe0b52ebcabe150445a68799689aaed7
Ben <blhutchings@upei.ca>
2023-07-05 10:05:52 -0300

    Updated package

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import GoogleServiceLogin from "./GoogleServiceLogin"
+import GoogleServiceLogin from "./GoogleServiceLogin";
 
 export default GoogleServiceLogin;
-export { GoogleServiceLogin }
+export { GoogleServiceLogin };
 
-export * from './browser/BrowserController'
-export * from './cookies/ICookieStore'
-export * from './RequestSession'
-export * from './RequestContext'
-export { ActionHandler, ActionHandlerRequest } from './handlers/abstract/AbstractActionHandler'
-export { TOTPActionHandler } from './handlers/verification/TOTPVerification'
-export { NormalCaptchaActionHandler, NormalCaptchaResponse } from './handlers/captcha/NormalCaptcha'
-export { ReCaptchaActionHandler, ReCaptchaResponse } from './handlers/captcha/ReCaptcha'
+export * from "./browser/BrowserController";
+export * from "./cookies/ICookieStore";
+export * from "./RequestSession";
+export * from "./RequestContext";
+export { ActionHandler, ActionHandlerRequest } from "./handlers/abstract/AbstractActionHandler";
+export { TOTPActionHandler } from "./handlers/verification/TOTPVerification";
+export { NormalCaptchaActionHandler, NormalCaptchaResponse } from "./handlers/captcha/NormalCaptcha";
+export { ReCaptchaActionHandler, ReCaptchaResponse } from "./handlers/captcha/ReCaptcha";
 
-export { GoogleServiceError, GoogleServiceLoginError, GoogleServiceLoginUndefinedError } from './utils/LoginError';
-export * from './types/LoginErrorStatus'
-export * from './types/LoginRequest'
-export * from './types/LoginResponse'
-export * from './types/PageState'
+export { GoogleServiceError, GoogleServiceLoginError, GoogleServiceLoginUndefinedError } from "./utils/LoginError";
+export * from "./types/LoginErrorStatus";
+export * from "./types/LoginRequest";
+export * from "./types/LoginResponse";
+export * from "./types/PageState";
