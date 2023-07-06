4b7e808732242eb7160cda566fe2d3361894e1bb
Ben <blhutchings@upei.ca>
2023-07-06 10:50:23 -0300

    Removed ResumeHandler

--- src/RequestSession.ts
+++ src/RequestSession.ts
@@ -1,5 +1,4 @@
 import AbstractHandler from "./handlers/abstract/AbstractHandler";
-import RetryHandler from "./handlers/session/ResumeHandler";
 import RequestContext from "./RequestContext";
 import { LoginResponse } from "./types/LoginResponse";
 
@@ -7,21 +6,15 @@ import { LoginResponse } from "./types/LoginResponse";
 
 export default class RequestSession {
 	private startHandler: AbstractHandler;
-	private retryHandler: RetryHandler;
 	private requestContext: RequestContext;
 
-	constructor(startHandler: AbstractHandler, retryHandler: RetryHandler, requestContext: RequestContext) {
+	constructor(startHandler: AbstractHandler, requestContext: RequestContext) {
 		this.startHandler = startHandler;
-		this.retryHandler = retryHandler;
 		this.requestContext = requestContext;
 	}
 
 	start(): Promise<LoginResponse> {
 		return this.startHandler.handle(this.requestContext);
-	}
-
-	retry() {
-		return this.retryHandler.handle(this.requestContext);
 	}
 
 	getContext() {
